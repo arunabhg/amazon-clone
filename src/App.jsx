f4ac0c6613c892d4df2f4103c2e4aaa450e8bc71
Arunabh Ghosh <arunabh.tech@gmail.com>
2022-04-16 00:23:44 +0530

    Added banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header";
+import Home from "./pages/home";
 import "./App.scss";
 
 function App() {
 	return (
 		<BrowserRouter>
+			<Header />
 			<Routes>
-				<Route path="/" element={<Header />} />
+				<Route path="/" element={<Home />} />
 			</Routes>
 		</BrowserRouter>
 	);

6f54e13353b3cef987f4d8ec8caa10b747aa8554
Arunabh Ghosh <arunabh.tech@gmail.com>
2022-04-17 00:10:05 +0530

    Initial commit

---
+++ src/components/general/Rating.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import StarIcon from "@mui/icons-material/Star";
+import StarOutlineIcon from "@mui/icons-material/StarOutline";
+
+import "./Rating.scss";
+
+function Rating({ rating, maxRating }) {
+	return (
+		<div className="rating">
+			{new Array(maxRating).fill(0).map((_, index) => {
+				const isActive = rating >= index + 1;
+				return (
+					<StarIcon
+						key={index}
+						className={classNames("rating__star", {
+							"rating__star--active": isActive
+						})}
+						fontSize="large"
+						path={isActive ? StarIcon : StarOutlineIcon}
+					/>
+				);
+			})}
+		</div>
+	);
+}
+
+export default Rating;

9a2a241a17f894d67950cac43165305961f8f97c
Arunabh Ghosh <arunabh.tech@gmail.com>
2022-04-19 22:55:30 +0530

    Updated with provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { CartProvider } from "./cart-context";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />		
+		<CartProvider>
+			<App />
+		</CartProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );

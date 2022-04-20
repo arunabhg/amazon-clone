ac77aab90d833d123578d318c8e30f4a8a80e492
Arunabh Ghosh <arunabh.tech@gmail.com>
2022-04-20 22:53:46 +0530

    Added Routes for Product, Login & Cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 import Home from "./pages/home";
+import Product from "./pages/product";
+import LoginPage from "./pages/login";
+import CartPage from "./pages/cart";
 import "./App.scss";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-			</Routes>
+			<div className="page-container">
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+				<Routes>
+					<Route path="/product/:productId" element={<Product />} />
+				</Routes>
+				<Routes>
+					<Route path="/login" element={<LoginPage />} />
+				</Routes>
+				<Routes>
+					<Route path="/cart" element={<CartPage />} />
+				</Routes>
+			</div>
 		</BrowserRouter>
 	);
 }
